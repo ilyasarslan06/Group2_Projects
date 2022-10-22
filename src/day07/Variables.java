a0718ac77dde20c0c01248a32460bd312f72b349
İlyas ARSLAN <ilyasarslan93@gmail.com>
2022-10-22 15:20:11 +0300

    Group2 Prejects have been created.

--- src/day07/Variables.java
+++ src/day07/Variables.java
@@ -4,7 +4,7 @@ public class Variables {
     public static void main(String[] args) {
         //= operatörü (assigment) sağında bulunan sabit bir değer (literal) veya bir işlemin sonucunu
         // solundaki değişkenin (variable) temsil ettiği hafoza bölgesini kaydeder.
-        int yas=40; // bu satır bir java ifadesidir (statement)
+        int yas=40; //bu satır bir java ifadesidir (statement)
 
         // Karenin alanını ve çevresini hesaplayınız.
          int kenar= 5;
